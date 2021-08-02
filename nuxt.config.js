384ff5060c86c1806aaca4e9d3b6a7b384d7099a
Timur Brachkow <brachkow@fastmail.com>
2021-08-03 00:48:21 +0300

    feat: style resources added

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,4 +13,8 @@ export default {
     port: 1111,
     host: '0.0.0.0',
   },
+  buildModules: ['@nuxtjs/style-resources'],
+  styleResources: {
+    scss: ['@/assets/styles/_variables.scss', '@/assets/styles/_mixins.scss'],
+  },
 };
